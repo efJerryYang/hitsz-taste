ba69a7cc2c0d219dd25d1606ca4e22e6ec556503
Jerry Yang <efJerryYang@outlook.com>
2022-12-13 00:03:13 +0800

    fix: dependency for slf4j logging

--- src/main/java/me/efjerryyang/webserver/util/CryptoUtilAES.java
+++ src/main/java/me/efjerryyang/webserver/util/CryptoUtilAES.java
@@ -2,6 +2,7 @@ package me.efjerryyang.webserver.util;
 
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
