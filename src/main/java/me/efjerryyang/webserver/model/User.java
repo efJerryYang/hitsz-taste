03d4f5958e4828db2c8589d74f9c1e913f9f3562
Jerry Yang <efJerryYang@outlook.com>
2022-12-12 18:17:11 +0800

    refactor: simplify User.java using Lombok

--- src/main/java/me/efjerryyang/webserver/model/User.java
+++ src/main/java/me/efjerryyang/webserver/model/User.java
@@ -1,12 +1,11 @@
 package me.efjerryyang.webserver.model;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
-import java.util.Objects;
-
-@Getter
-@Setter
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
 public class User {
     private Long userId;
     private String name;
@@ -14,39 +13,4 @@ public class User {
     private String password;
     private String address;
     private String email;
-
-    public User() {
-    }
-
-    public User(Long userId, String name, String phone, String password, String address, String email) {
-        this.userId = userId;
-        this.name = name;
-        this.phone = phone;
-        this.password = password;
-        this.address = address;
-        this.email = email;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-        User user = (User) o;
-        return Objects.equals(userId, user.userId) &&
-                Objects.equals(name, user.name) &&
-                Objects.equals(email, user.email) &&
-                Objects.equals(password, user.password) &&
-                Objects.equals(phone, user.phone) &&
-                Objects.equals(address, user.address);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(userId, name, email, password, phone, address);
-    }
-
 }
