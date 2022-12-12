483ec4ec1ccc88033d3569900810da4b95101c1b
Jerry Yang <efJerryYang@outlook.com>
2022-12-12 11:44:56 +0800

    feat: update python script to encrypt password
    更新了 application.properties 的格式，更新了脚本提供的提示

--- scripts/encrypt_password.py
+++ scripts/encrypt_password.py
@@ -2,11 +2,15 @@ import os
 import sys
 import getpass
 import base64
-import random
 from Crypto.Cipher import AES
 from Crypto.Util.Padding import pad
 
 # Prompt the user for their database password
+print("+--------+")
+print("| Notice |")
+print("+--------+")
+print("    We are assuming that you use MySQL as the database server, so the generated template has default values for MySQL. \n\n    If you are not using MySQL, you should modify the 'application.properties' file manually according to your database settings.\n")
+username = input("Enter your database username: ")
 password = getpass.getpass("Enter your database password: ")
 
 # Confirm the password by asking the user to enter it again
@@ -25,29 +29,30 @@ cipher = AES.new(key, mode=AES.MODE_CBC, iv=iv)
 encrypted_password = base64.b64encode(cipher.encrypt(pad(password.encode("utf-8"), 16, 'pkcs7')))
 
 # Print the key and encrypted password to the terminal
-print(f"Key: {base64.b64encode(key).decode('utf-8')}")
 print(f"Encrypted password: {encrypted_password.decode('utf-8')}")
 
 # Store the encrypted password in an environment variable
 os.environ["ENCRYPTED_PASSWORD"] = encrypted_password.decode('utf-8')
 
 # Create a template for the application.properties file
-template = """
+template = """# Web server settings
 server.port=8080
 server.servlet.context-path=/hitsz-taste
 
 # Database connection settings
 spring.datasource.url=jdbc:mysql://localhost:3306/hitsz_taste
-spring.datasource.username=root
-spring.datasource.password=ENC(#{env.ENCRYPTED_PASSWORD})
+""" + f"spring.datasource.username={username}" +"""
 spring.datasource.driver-class-name=com.mysql.cj.jdbc.Driver
 
+# Password decryption parameters
+""" + f"spring.datasource.key={base64.b64encode(key).decode('utf-8')}\n" + f"spring.datasource.iv={base64.b64encode(iv).decode('utf-8')}\n" + """
 # MySQL JDBC driver settings
 spring.jpa.database-platform=org.hibernate.dialect.MySQL8Dialect
 spring.jpa.hibernate.ddl-auto=none
-""" + f"spring.datasource.key={base64.b64encode(key).decode('utf-8')}\n" + f"spring.datasource.iv={base64.b64encode(iv).decode('utf-8')}\n"
+"""
 
 # Write the template to the application.properties file
 with open("application.properties", "w") as f:
     f.write(template)
 
+print("\nNow, you can move the file 'application.properties' to the classpath 'src/main/resources/'.\n\nAnd you should guarantee the encrypted password to be set to the environment variable 'ENCRYPTED_PASSWORD' on your operating system.\n")
