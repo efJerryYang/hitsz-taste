44e8751494442b03a8005e444990d9ae3c1ed520
Jerry Yang <efJerryYang@outlook.com>
2022-12-20 23:13:00 +0800

    feat: make client and server create the same hash
    注意：当前结果无法检验 JavaScript 是否被执行成功，使用的 acceptHeader 检查不能看到 javascript 字段。临时使用了能够得到的 header 在火狐和谷歌浏览器上的不同来进行 js 判断的测试
    
    该版本是可以检查加密后的密码等结果的，后面的更新需要移除相关的 logging
    
    目前检查确保了 JavaScript 在客户端计算的结果和 Java 在服务端计算的结果是统一的，为了便于验证统一性，第一次对密码的 hash 是使用 16 进制表示的。

--- src/main/java/me/efjerryyang/webserver/util/CryptoUtilHash.java
+++ src/main/java/me/efjerryyang/webserver/util/CryptoUtilHash.java
@@ -1,21 +1,33 @@
 package me.efjerryyang.webserver.util;
 
 import org.bouncycastle.jcajce.provider.digest.SHA256;
+import org.bouncycastle.util.encoders.Hex;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Base64;
 import java.util.Random;
 
 public class CryptoUtilHash {
+    public static final int SALT_LENGTH = 16;
     public static Logger logger = LoggerFactory.getLogger(CryptoUtilHash.class);
 
-    public static String generateSalt() {
+    public static String getSalt() {
         logger.info("Generating salt");
         // generate a random salt
         Random random = new Random();
-        byte[] salt = new byte[16];
+        byte[] salt = new byte[SALT_LENGTH];
         random.nextBytes(salt);
-        return new String(salt);
+        return Hex.toHexString(salt);
+    }
+
+    public static String hash(String password) {
+        logger.info("Hashing password string");
+        // use sha256 to hash the password
+        // it can be either a plain text or a sha256 hash
+        SHA256.Digest digest = new SHA256.Digest();
+        // use hex string to represent the hash
+        return Hex.toHexString(digest.digest(password.getBytes()));
     }
 
     public static String hashWithSalt(String password, String salt) {
@@ -26,37 +38,22 @@ public class CryptoUtilHash {
         if (!password.matches(hashRegex)) {
             // if it is not a valid sha256 hash
             Exception e = new IllegalArgumentException();
-            logger.error("Invalid password hash", e);
+            logger.error("Invalid password hash: " + password, e);
             throw new IllegalArgumentException("Invalid password hash", e);
         }
         logger.info("Password is a valid sha256 hash");
         // do xor with password and salt
         // then hash the result
-        byte[] passwordBytes = password.getBytes();
-        byte[] saltBytes = salt.getBytes();
+        byte[] passwordBytes = Hex.decode(password);
+        byte[] saltBytes = Hex.decode(salt);
         byte[] xorBytes = new byte[passwordBytes.length];
         for (int i = 0; i < passwordBytes.length; i++) {
             xorBytes[i] = (byte) (passwordBytes[i] ^ saltBytes[i % saltBytes.length]);
         }
-        logger.info("hashing the result");
-        return hash(xorBytes);
+        logger.info("hash with salt, producing the result...");
+        // use base64 to encode the string, so that get the conventional format
+        SHA256.Digest digest = new SHA256.Digest();
+        return Base64.getEncoder().encodeToString(digest.digest(xorBytes));
     }
 
-    public static String hash(byte[] password) {
-        logger.info("Hashing password bytes");
-        // use sha256 to hash the password
-        // it can be either a plain text or a sha256 hash
-        // byte number of a sha256 hash is 32
-        SHA256.Digest digest = new SHA256.Digest();
-        return new String(digest.digest(password));
-    }
-
-    public static String hash(String password) {
-        logger.info("Hashing password string");
-        // use sha256 to hash the password
-        // it can be either a plain text or a sha256 hash
-        SHA256.Digest digest = new SHA256.Digest();
-        byte[] hash = digest.digest(password.getBytes());
-        return new String(hash);
-    }
 }
