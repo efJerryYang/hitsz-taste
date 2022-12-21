cea16bf57b2d030b4e5ebc6b818a2ee521dec3c4
Jerry Yang <efJerryYang@outlook.com>
2022-12-21 23:17:35 +0800

    fix: typo

--- src/main/java/me/efjerryyang/webserver/service/ValidationService.java
+++ src/main/java/me/efjerryyang/webserver/service/ValidationService.java
@@ -154,7 +154,7 @@ public class ValidationService {
         // chrome:text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9
         // firefox: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8
         if (acceptHeader != null && (acceptHeader.contains("application/javascript") || jsEnabled.equals("true"))) {
-            logger.info("accept Heade: {}, jsEnabled: {}", acceptHeader, jsEnabled);
+            logger.info("accept Header: {}, jsEnabled: {}", acceptHeader, jsEnabled);
             // JavaScript is likely enabled on the client
             logger.info("javascript is enabled");
             return true;
