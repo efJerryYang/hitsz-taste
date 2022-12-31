9f3dcbe41631786b64aebe0e16b04f8197b8313e
Jerry Yang <efJerryYang@outlook.com>
2022-12-31 14:06:49 +0800

    feat: update Controller for search and filter

--- src/main/java/me/efjerryyang/webserver/controller/SearchController.java
+++ src/main/java/me/efjerryyang/webserver/controller/SearchController.java
@@ -1,8 +1,10 @@
 package me.efjerryyang.webserver.controller;
 
 import me.efjerryyang.webserver.model.Dish;
-import me.efjerryyang.webserver.service.DishService;
-import me.efjerryyang.webserver.service.ValidationService;
+import me.efjerryyang.webserver.model.Merchant;
+import me.efjerryyang.webserver.service.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,16 +12,24 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Controller
 public class SearchController {
-    private DishService dishService;
-    private ValidationService validationService;
+    private static final Logger logger = LoggerFactory.getLogger(SearchController.class);
+    private final CafeteriaService cafeteriaService;
+    private final MerchantService merchantService;
+    private final DishService dishService;
+    private final ValidationService validationService;
+    private final ContractService contractService;
 
     @Autowired
-    public SearchController(DishService dishService, ValidationService validationService) {
+    public SearchController(CafeteriaService cafeteriaService, DishService dishService, ValidationService validationService, MerchantService merchantService, ContractService contractService) {
+        this.cafeteriaService = cafeteriaService;
         this.dishService = dishService;
         this.validationService = validationService;
+        this.merchantService = merchantService;
+        this.contractService = contractService;
     }
 
     @GetMapping("/search")
@@ -43,5 +53,35 @@ public class SearchController {
         System.out.printf(searchResults.toString());
         return "home";
     }
+
+    @GetMapping("/filter")
+    public String filter(@RequestParam(value = "cafeteria", required = false) Long cafeteriaId, @RequestParam(value = "merchant", required = false) Long merchantId, Model model) {
+        // Retrieve the list of merchants from the database
+        List<Merchant> merchants = merchantService.getAll();
+
+        // If a cafeteria and merchant are selected, filter the list of merchants by both cafeteria and merchant
+        if (cafeteriaId != null && merchantId != null) {
+            merchants = merchants.stream()
+                    .filter(merchant -> cafeteriaService.getAllByIds(contractService.getCafeteriaIdsByMerchantId(merchant.getMerchantId())).contains(cafeteriaService.getById(cafeteriaId))
+                            && merchant.getMerchantId().equals(merchantId))
+                    .collect(Collectors.toList());
+        }
+        // If only a cafeteria is selected, filter the list of merchants by cafeteria
+        else if (cafeteriaId != null) {
+            merchants = merchants.stream()
+                    .filter(merchant -> cafeteriaService.getAllByIds(contractService.getCafeteriaIdsByMerchantId(merchant.getMerchantId())).contains(cafeteriaService.getById(cafeteriaId)))
+                    .collect(Collectors.toList());
+        }
+        // If only a merchant is selected, filter the list of merchants by merchant
+        else if (merchantId != null) {
+            merchants = merchants.stream().filter(merchant -> merchant.getMerchantId().equals(merchantId)).collect(Collectors.toList());
+        }
+
+        // Add the filtered list of merchants to the model
+        model.addAttribute("merchants", merchants);
+        // Render the template with the filtered list of merchants
+        return "home";
+    }
+
 }
 
