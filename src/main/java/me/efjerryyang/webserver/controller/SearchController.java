8d294d0a2efbcfd8cf7a106a83e6d306bfd995ce
Jerry Yang <efJerryYang@outlook.com>
2023-01-06 22:40:04 +0800

    fix: detect jsEnabled in home page

--- src/main/java/me/efjerryyang/webserver/controller/SearchController.java
+++ src/main/java/me/efjerryyang/webserver/controller/SearchController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
@@ -33,7 +34,6 @@ public class SearchController {
         this.contractService = contractService;
         this.filterService = filterService;
     }
-
     @GetMapping("/home/search")
     public String search(@RequestParam("query") String query, Model model) {
         String updatedQuery;
