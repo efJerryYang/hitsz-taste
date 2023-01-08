6e229329057cc69ca40e0c32115c3fe7c809164a
Jerry Yang <efJerryYang@outlook.com>
2023-01-09 01:29:05 +0800

    fix: sanitization mistakes in regular expression

--- src/main/resources/static/sanitize.js
+++ src/main/resources/static/sanitize.js
@@ -1,11 +1,15 @@
 function sanitizeSearchQuery(query) {
     // Remove any HTML tags from the query
+    console.log("Before removing HTML tags: " + query);
     query = query.replace(/<[^>]*>/g, "");
     // Remove any JavaScript event handlers from the query
+    console.log("Before removing JavaScript Event Handlers: " + query);
     query = query.replace(/on\w+\s*=\s*("|')[^"']*("|')/g, "");
     // Remove any potentially malicious characters from the query
-    query = query.replace(/[^\w\s-\u4e00-\u9fa5]/g, "");
+    console.log("Before removing potentially malicious characters: " + query);
+    query = query.replace(/[^[\w\s-\u4e00-\u9fa5]]/g, "");
     // Trim leading and trailing whitespace
+    console.log("Before trimming the query: " + query);
     query = query.trim();
     // Check if the query contains any SQL injection keywords
     const injectionKeywords = ["SELECT", "INSERT", "UPDATE", "DELETE", "CREATE", "DROP", "ALTER", "TRUNCATE", "UNION"];
@@ -15,9 +19,11 @@ function sanitizeSearchQuery(query) {
         }
     }
     // Escape special characters
+    console.log("Before escaping special characters: " + query);
     query = query.replace(/'/g, "\\'");
     query = query.replace(/"/g, "\\\"");
     query = query.replace(/\\/g, "\\\\");
     // Return the sanitized query
+    console.log("Processed query: " + query);
     return query;
 }
