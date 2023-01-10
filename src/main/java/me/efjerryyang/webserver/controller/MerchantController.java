a76b6c2f26694249ccbb5c86743b6004b2d73894
Jerry Yang <efJerryYang@outlook.com>
2023-01-10 17:08:10 +0800

    feat: add merchant name to dashboard

--- src/main/java/me/efjerryyang/webserver/controller/MerchantController.java
+++ src/main/java/me/efjerryyang/webserver/controller/MerchantController.java
@@ -1,7 +1,9 @@
 package me.efjerryyang.webserver.controller;
 
 import jakarta.servlet.http.HttpSession;
+import me.efjerryyang.webserver.model.Dish;
 import me.efjerryyang.webserver.model.Merchant;
+import me.efjerryyang.webserver.model.Order;
 import me.efjerryyang.webserver.model.User;
 import me.efjerryyang.webserver.service.*;
 import org.slf4j.Logger;
@@ -10,6 +12,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class MerchantController {
@@ -22,6 +26,9 @@ public class MerchantController {
     private final MerchantUserService merchantUserService;
     @Autowired
     private HttpSession session;
+
+    private List<Dish> dishList;
+    private List<Order> orderList;
 
     @Autowired
     public MerchantController(MerchantService merchantService, UserService userService, UserRoleService userRoleService, OrderService orderService, DishService dishService, MerchantUserService merchantUserService) {
@@ -40,8 +47,9 @@ public class MerchantController {
             model.addAttribute("username", username);
             User user = userService.getByUsername(username);
             Merchant merchant = null;
+
             try {
-                 merchant = merchantService.getById(merchantUserService.getMerchantIdByUserId(user.getUserId()));
+                merchant = merchantService.getById(merchantUserService.getMerchantIdByUserId(user.getUserId()));
                 if (merchant == null) {
                     logger.error("Not a merchant role of user: {}", username);
                     return "redirect:/dashboard";
@@ -51,12 +59,17 @@ public class MerchantController {
                 return "redirect:/dashboard";
             }
             if (merchantUserService.getMerchantIdByUserId(user.getUserId()) != null) {
-                model.addAttribute("merchant", merchant);
+//                model.addAttribute("merchant", merchant);
+                model.addAttribute("merchantName", merchant.getName());
                 // TODO: to be fixed, current order structure is not correct, merchants received should be suborders
                 //  Cart (customer) -> order (cafeteria) -> suborder (merchant)
                 //  So now restrict the order to only contains one merchant
-                model.addAttribute("dishList", dishService.getAllByMerchantId(merchant.getMerchantId()));
-                model.addAttribute("orderList", orderService.getAllByMerchantId(merchant.getMerchantId()));
+                dishList = dishService.getAllByMerchantId(merchant.getMerchantId());
+                orderList = orderService.getAllByMerchantId(merchant.getMerchantId());
+                model.addAttribute("dishList", dishList);
+                model.addAttribute("pendingOrderList", orderList.stream().filter(order -> order.getStatus().equals("pending")).toList());
+                model.addAttribute("processingOrderList", orderList.stream().filter(order -> order.getStatus().equals("processing")).toList());
+                model.addAttribute("completedOrderList", orderList.stream().filter(order -> order.getStatus().equals("completed")).toList());
                 return "dashboard_merchant";
             } else {
                 return "redirect:/dashboard";
